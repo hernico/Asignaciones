6f8c7280c081bf57bfa4ed9eadc3ea157abd5353
hernico <hernico@gmail.com>
2023-02-20 11:22:11 -0500

    Tarea Sensei

--- Ninja/Ninja.js
+++ Ninja/Ninja.js
@@ -3,8 +3,8 @@ constructor(nombre, salud, velocidad, fuerza) {
     console.log('constructor');
 this.nombre = nombre;
 this.salud = salud;
-this.velocidad = 3;
-this.fuerza = 3; 
+this.velocidad = velocidad;
+this.fuerza = fuerza; 
 }
 
 sayName () {
@@ -14,9 +14,9 @@ sayName () {
 
 showStats () {
     console.log("Stats del Ninja");
-    console.log("Fuerza: " + this.fuerza )
-    console.log("Velocidad: " + this.velocidad )
     console.log("Salud: " + this.salud )
+    console.log("Velocidad: " + this.velocidad )
+    console.log("Fuerza: " + this.fuerza )
 }
 
 drinkSake(){
@@ -26,10 +26,29 @@ drinkSake(){
 }
 
 }   
-const ninja1 = new Ninja("Hyabusa",10);
+const ninja1 = new Ninja("Hyabusa",10, 3 ,3);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake(); 
 
  
-// parte 2
+console.log('parte 2');
+
+class Sensei extends Ninja{
+constructor (name, salud, velocidad, fuerza) {
+    super(name, salud, velocidad, fuerza);
+    this.sabiduria = 10;
+}  
+
+speakWisdom() { 
+
+console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses");
+}
+
+}
+
+const superSensei = new Sensei("Master Splinter",200,10,10);
+superSensei.drinkSake();
+superSensei.speakWisdom();
+superSensei.sayName();
+superSensei.showStats();
