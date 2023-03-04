523477133923dee134f5e76a70cb5f3d40f2b073
hernico <hernico@gmail.com>
2023-03-04 15:13:42 -0500

    First React

---
+++ mezcla/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+  
+        <div>
+  <div class="item1">Header</div>
+  <div class="item2">Menu</div>
+  <div class="item3">Main</div>  
+  <div class="item4">Right</div>
+  <div class="item5">Footer</div>
+</div>
+</div>
+  );
+
+}
+export default App;
